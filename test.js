4e456b2f1103de02bb46bca2c2d9e2d3fbcb7da6
Onwuasoanya James <onwuasoanyajames@gmail.com>
2021-06-03 17:07:22 +0100

    circle-menus

--- test.js
+++ test.js
@@ -1,19 +1,5 @@
-const list = ['apple', 'banana', 'bitruit', 'waterMelon']
-const items = {
-    car: 'ferrari',
-    gun: 'M4 carbine',
-    utensils: ['spoon', 'tea-spoon', 'table-knife', 'fork']
+//const express = require('express')
+const app = express()
 
-}
-console.log(items.utensils[1])
-console.log(list.length)
-
-// const http = require('http')
-// const port = 8080
-
-
-// http.createServer((req, res)=> {
-//     res.writeHead (200, {'Content-type':'text/plain'});
-//     res.write('Hello World! and /n');
-//     res.end()
-// }).listen(port, ()=> {console.log(`server listening on port ${port}`)})
+// app.listen(process.env.PORT, ()=> {`listening for requests on specified port`})
+// app.use(express.static('test.html'))
